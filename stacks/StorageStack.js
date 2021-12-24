495d876a32709b7675f8182d7bf500e030b99bfa
Adibta Triantama <adibtatriantama@gmail.com>
2021-12-24 15:04:18 +0700

    style: format using prettier

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,23 +1,23 @@
 import * as sst from "@serverless-stack/resources";
 
-export default class StorageStack extends sst.Stack { 
-    bucket;
-    table;
+export default class StorageStack extends sst.Stack {
+  bucket;
+  table;
 
-    constructor(scope, id, props) { 
-        super(scope, id, props);
+  constructor(scope, id, props) {
+    super(scope, id, props);
 
-        this.bucket = new sst.Bucket(this, "Uploads");
+    this.bucket = new sst.Bucket(this, "Uploads");
 
-        this.table = new sst.Table(this, "Notes", { 
-            fields: {
-                userId: sst.TableFieldType.STRING,
-                noteId: sst.TableFieldType.STRING, 
-            },
-            primaryIndex: { 
-                partitionKey: "userId", 
-                sortKey: "noteId" 
-            }, 
-        });
-    } 
+    this.table = new sst.Table(this, "Notes", {
+      fields: {
+        userId: sst.TableFieldType.STRING,
+        noteId: sst.TableFieldType.STRING,
+      },
+      primaryIndex: {
+        partitionKey: "userId",
+        sortKey: "noteId",
+      },
+    });
+  }
 }
