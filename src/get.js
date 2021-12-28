d810bc3b42ec91c2ca99e384689c10e0e0eb632b
Adibta Triantama <adibtatriantama@gmail.com>
2021-12-28 11:00:19 +0700

    feat: secure the api

--- src/get.js
+++ src/get.js
@@ -5,11 +5,11 @@ export const main = handler(async (event) => {
   const params = {
     TableName: process.env.TABLE_NAME,
     Key: {
-      userId: "123",
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
       noteId: event.pathParameters.id,
     },
   };
-  
+
   const result = await dynamoDb.get(params);
 
   if (!result.Item) {
